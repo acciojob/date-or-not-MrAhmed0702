05bc5b5addefda2193b71a4ddc6e6dd1f4e4952e
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-11-19 23:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var isDate = function (input) {
-  //   write your code here
+  if (Object.prototype.toString.call(input) === '[object Date]') {
+    return !isNaN(input.getTime());
+  }
+  const parsedDate = new Date(input);
+  return !isNaN(parsedDate.getTime());
 };
+
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
